f3cb51712529b69ee50b566cea9a0d342354939f
Lachlan Gibson <69880787+LachlanGibson@users.noreply.github.com>
2023-11-30 15:59:30 +1000

    recreate navbar and homepage in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./navigation/Navbar";
+import Home from "./home/Home";
+import About from "./about/About";
+import Blog from "./blog/Blog";
+import Footer from "./footer/Footer";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC<{}> = () => {
+  return (
+    <footer>
+      <span>&copy; Copyright 2023 Lachlan Gibson</span>
+    </footer>
+  );
+};
+
+export default Footer;
