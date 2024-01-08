3451f35cf9d74bca5725b731df8825179e237ec6
Lachlan Gibson <69880787+LachlanGibson@users.noreply.github.com>
2024-01-08 18:01:47 +1000

    update connect four css

--- src/games/connectfour/GameConnectFour.tsx
+++ src/games/connectfour/GameConnectFour.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import "./GameConnectFour.css";
+import React, { useState } from "react";
+import styles from "./GameConnectFour.module.css";
 
 const GameConnectFour: React.FC = () => {
   // note that the board array elements are in the order of columns, not rows
-  const [board, setBoard] = React.useState<string[][]>([
+  const [board, setBoard] = useState<string[][]>([
     ["", "", "", "", "", ""],
     ["", "", "", "", "", ""],
     ["", "", "", "", "", ""],
@@ -12,20 +12,34 @@ const GameConnectFour: React.FC = () => {
     ["", "", "", "", "", ""],
     ["", "", "", "", "", ""],
   ]);
+  const [slotAbove, setSlotAbove] = useState(["", "", "", "", "", "", ""]);
 
   return (
     <>
       <p>This is a work in progress</p>
-      <div className="game-board">
-        {board.map((column, columnIndex) => {
-          return (
-            <div key={columnIndex} className="column">
-              {column.map((cell, cellIndex) => {
-                return <div key={cellIndex} className="cell"></div>;
-              })}
+      <div className={styles.gameBoard}>
+        <div className={styles.inputRow}>
+          {slotAbove.map((cell, cellIndex) => (
+            <div key={-1 - cellIndex} className={styles.slotCell}>
+              <div className={styles.circle}></div>
             </div>
-          );
-        })}
+          ))}
+        </div>
+        <div className={styles.slots}>
+          {board.map((column, columnIndex) => {
+            return (
+              <div key={columnIndex} className={styles.column}>
+                {column.map((cell, cellIndex) => {
+                  return (
+                    <div key={cellIndex} className={styles.cell}>
+                      <div className={styles.circle}></div>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
