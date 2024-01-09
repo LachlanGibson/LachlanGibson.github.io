632f633fe436dac7a2350183d7f20632247f7000
Lachlan Gibson <69880787+LachlanGibson@users.noreply.github.com>
2024-01-10 09:50:52 +1000

    add minimax AI

--- src/games/connectfour/GameConnectFour.tsx
+++ src/games/connectfour/GameConnectFour.tsx
@@ -2,8 +2,60 @@ import React, { useEffect, useState } from "react";
 import styles from "./GameConnectFour.module.css";
 
 type GameStatus = "in progress" | "R wins" | "Y wins" | "tie";
+type Player = "R" | "Y";
 
 const emptyRow = ["", "", "", "", "", "", ""];
+
+const winPossibilities = [] as number[][][];
+for (let col = 0; col < 7; col++) {
+  for (let row = 0; row < 3; row++) {
+    winPossibilities.push([
+      [col, row],
+      [col, row + 1],
+      [col, row + 2],
+      [col, row + 3],
+    ]);
+  }
+}
+for (let col = 0; col < 4; col++) {
+  for (let row = 0; row < 6; row++) {
+    winPossibilities.push([
+      [col, row],
+      [col + 1, row],
+      [col + 2, row],
+      [col + 3, row],
+    ]);
+  }
+}
+for (let col = 0; col < 4; col++) {
+  for (let row = 0; row < 3; row++) {
+    winPossibilities.push([
+      [col, row],
+      [col + 1, row + 1],
+      [col + 2, row + 2],
+      [col + 3, row + 3],
+    ]);
+    winPossibilities.push([
+      [6 - col, row],
+      [5 - col, row + 1],
+      [4 - col, row + 2],
+      [3 - col, row + 3],
+    ]);
+  }
+}
+
+const evaluateMemo = {} as { [key: string]: number };
+
+const stringifyBoard = (board: string[][]): string => {
+  let transposedBoard = board[0].map((_, colIndex) =>
+    board.map((row) => row[colIndex])
+  );
+  return transposedBoard
+    .map((col) => {
+      return col.map((cell) => (cell === "" ? "_" : cell)).join("");
+    })
+    .join("\n");
+};
 
 const GameConnectFour: React.FC = () => {
   // note that the board array elements are in the order of columns, not rows
@@ -17,8 +69,26 @@ const GameConnectFour: React.FC = () => {
     ["", "", "", "", "", ""],
   ]);
   const [slotAbove, setSlotAbove] = useState([...emptyRow]);
-  const [player, setPlayer] = useState<"R" | "Y">("R");
+  const [player, setPlayer] = useState<Player>("R");
   const [gameStatus, setgameStatus] = useState<GameStatus>("in progress");
+  const [noise, setNoise] = useState<number>(0);
+  const [depth, setDepth] = useState<number>(4);
+  const [isRAI, setIsRAI] = useState<boolean>(false);
+  const [isYAI, setIsYAI] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (gameStatus !== "in progress") {
+      return;
+    }
+    if (player === "R" && isRAI) {
+      const move = aiChooseMove();
+      makeMove(move);
+    } else if (player === "Y" && isYAI) {
+      const move = aiChooseMove();
+      makeMove(move);
+    }
+    console.log(evaluateMemo);
+  }, [player, gameStatus, isRAI, isYAI]);
 
   const handleMouseEnter = (columnIndex: number) => {
     if (gameStatus !== "in progress") {
@@ -160,6 +230,105 @@ const GameConnectFour: React.FC = () => {
     ]);
     setPlayer("R");
     setgameStatus("in progress");
+  };
+
+  const minimax = (
+    prevBoard: string[][],
+    action: number,
+    depth: number,
+    alpha: number,
+    beta: number,
+    player: Player,
+    noise?: number
+  ) => {
+    const board = thinkStep(prevBoard, action);
+    const result = checkWin(board, action);
+
+    if (result === "R wins") {
+      return 1;
+    } else if (result === "Y wins") {
+      return -1;
+    } else if (result === "tie") {
+      return 0;
+    } else if (depth === 0) {
+      return evaluateBoard(board);
+    }
+
+    let bestScore = player === "R" ? -Infinity : Infinity;
+    const moves = availableMoves(board);
+    for (const action of moves) {
+      const nudge = noise ? (Math.random() - 0.5) * 2 * noise : 0;
+      const score =
+        nudge +
+        minimax(
+          board,
+          action,
+          depth - 1,
+          alpha,
+          beta,
+          player === "R" ? "Y" : "R",
+          noise
+        );
+      if (player === "R") {
+        bestScore = Math.max(bestScore, score);
+        alpha = Math.max(alpha, score);
+      } else {
+        bestScore = Math.min(bestScore, score);
+        beta = Math.min(beta, score);
+      }
+      if (alpha >= beta) {
+        break;
+      }
+    }
+    return bestScore;
+  };
+
+  const evaluateBoard = (board: string[][]) => {
+    const boardString = stringifyBoard(board);
+    if (evaluateMemo[boardString]) {
+      return evaluateMemo[boardString];
+    }
+    // count the difference in number of ways to connect four for each player
+    const count = winPossibilities.reduce((acc, winPossibility): number => {
+      if (winPossibility.every(([col, row]) => board[col][row] !== "Y")) {
+        acc++;
+      }
+      if (winPossibility.every(([col, row]) => board[col][row] !== "R")) {
+        acc--;
+      }
+      return acc;
+    }, 0);
+    evaluateMemo[boardString] = count / winPossibilities.length;
+    return evaluateMemo[boardString];
+  };
+
+  const aiChooseMove = () => {
+    const moves = availableMoves(board);
+    let bestScore = player === "R" ? -Infinity : Infinity;
+    let bestMove = moves[0];
+    for (const action of moves) {
+      const nudge = noise ? (Math.random() - 0.5) * 2 * noise : 0;
+      const score =
+        nudge +
+        minimax(
+          board,
+          action,
+          depth,
+          -Infinity,
+          Infinity,
+          player === "R" ? "Y" : "R",
+          noise
+        );
+      if (score > bestScore && player === "R") {
+        bestScore = score;
+        bestMove = action;
+      }
+      if (score < bestScore && player === "Y") {
+        bestScore = score;
+        bestMove = action;
+      }
+    }
+    return bestMove;
   };
 
   return (
