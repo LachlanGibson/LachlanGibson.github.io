d905f073dd522dc6774f3aa66785dc96c33de8c4
Lachlan Gibson <69880787+LachlanGibson@users.noreply.github.com>
2024-01-11 15:10:50 +1000

    change hydrate and render to hydrateRoot and createRoot for React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { hydrate, render } from "react-dom";
+import { hydrateRoot, createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
-if (rootElement && rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
+if (rootElement) {
+  if (rootElement.hasChildNodes()) {
+    hydrateRoot(rootElement, <App />);
+  } else {
+    const root = createRoot(rootElement);
+    root.render(<App />);
+  }
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
