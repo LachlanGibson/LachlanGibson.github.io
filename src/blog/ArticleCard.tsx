c5fc4d6f26d61651260429939a3953498c303653
Lachlan Gibson <69880787+LachlanGibson@users.noreply.github.com>
2024-01-16 20:52:59 +1000

    update styling to fix safari bug and light mode bug

--- src/blog/ArticleCard.tsx
+++ src/blog/ArticleCard.tsx
@@ -16,23 +16,17 @@ const ArticleCard = ({
 }) => {
   return (
     <>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
         <div className="rounded-t-lg aspect-[5/4] overflow-hidden relative">
           <Link to={`/blog/${slug}`}>
-            <img
-              src={imageLink}
-              alt=""
-              className="w-full relative top-1/3 -translate-y-1/3"
-            />
+            <img src={imageLink} alt="" className="w-full relative" />
           </Link>
         </div>
         <div className="px-2 pt-2 pb-1">
           <Link to={`/blog/${slug}`}>
-            <h5 className="text-lg tracking-tight text-gray-900 dark:text-white">
-              {title}
-            </h5>
+            <h5 className="text-lg tracking-tight text-white">{title}</h5>
           </Link>
-          <p className="font-normal text-gray-700 dark:text-gray-400 text-xs">
+          <p className="font-normal text-gray-400 text-xs">
             <span>
               <time dateTime={publishedDateISO}>{publishedDateLabel}</time>
             </span>
