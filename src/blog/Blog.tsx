b342b3bfaf3797d877df96d85d14b248c590e052
Lachlan Gibson <69880787+LachlanGibson@users.noreply.github.com>
2024-01-18 10:04:05 +1000

    add meta data

--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -4,11 +4,41 @@ import articleMetaData from "./articleMetaData";
 import ArticleCard from "./ArticleCard";
 import "./Blog.css";
 
+const name = "Lachlan Gibson";
+const title = "Lachlan Gibson - Blog";
+const description =
+  "My blog is where I show off some of my projects and write about topics that interest me.";
+const imageUrl =
+  "https://www.lachlangibson.dev/images/home/lachlan_gibson.webp";
+const pageUrl = "https://www.lachlangibson.dev/blog/";
+const keyWords = `Lachlan Gibson, data scientist, developer, AI, simulation, 
+machine learning, deep learning, reinforcement learning, dimensionality reduction, 
+algorithmic design, neural network architecture, Q-Learning, dynamic programming, 
+numerical methods, scientific computing, MATLAB, finite element method, 
+finite difference method, discrete event simulation, data analytics, 
+Python, R, SQL, PyTorch, pandas, Matplotlib, physics, mathematics, statistics, 
+blog, articles, projects, portfolio, projects, tic-tac-toe, connect four, 
+chatGPT, GPT-2, GPT-3, OpenAI, Google, TensorFlow`;
+
 const Blog: React.FC<{}> = () => {
   return (
     <>
       <Helmet>
-        <title>Lachlan Gibson - Blog</title>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta name="twitter:title" content={title} />
+        <meta name="author" content={name} />
+        <meta property="og:image" content={imageUrl} />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta property="og:url" content={pageUrl} />
+        <link rel="canonical" href={pageUrl} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content={keyWords} />
       </Helmet>
       <h1 className="text-xl font-bold mb-3">Blog</h1>
       <div className="blog-list-div">
